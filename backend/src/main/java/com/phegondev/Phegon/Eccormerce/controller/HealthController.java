87ea6ca8828c2ba9854b0b03d84bf052e06b7393
PC <kyf.hoang@vatek.asia>
2026-04-22 16:55:42 +0700

    update

--- backend/src/main/java/com/phegondev/Phegon/Eccormerce/controller/HealthController.java
+++ backend/src/main/java/com/phegondev/Phegon/Eccormerce/controller/HealthController.java
@@ -2,6 +2,9 @@ package com.phegondev.Phegon.Eccormerce.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class HealthController {
     @GetMapping("/health")
